bcd7578d72f89fa543edaa4c1553fe1516111189
jetson <jetson@car2.localdomain>
2024-11-05 14:48:29 -0800

    feat: update reverse tunnel and streaming scripts

--- reverse_tunnel_car2.sh
+++ reverse_tunnel_car2.sh
@@ -1,2 +1,2 @@
 # bind 8882 of remote to localhost:8888 where the jupyter notebook is running
-ssh -R localhost:8882:localhost:8888 amirmo@129.97.168.82 -N
+ssh -R localhost:8888:localhost:8888 hpc4 -N

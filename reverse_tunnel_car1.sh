b5a78f6c16f14ece3b16e61df5309bf02e9c3e24
kazem <kazem>
2024-11-12 14:02:35 -0500

    refactor: remove reverse tunnel scripts

--- reverse_tunnel_car1.sh
+++
@@ -1,2 +0,0 @@
-# bind 8881 of remote to localhost:8888 where the jupyter notebook is running
-ssh -R localhost:8888:localhost:8888 hpc1 -N
--- reverse_tunnel_car2.sh
+++
@@ -1,2 +0,0 @@
-# bind 8882 of remote to localhost:8888 where the jupyter notebook is running
-ssh -R localhost:8888:localhost:8888 hpc4 -N

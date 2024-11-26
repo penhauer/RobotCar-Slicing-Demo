008e9a6b59a65033050aa2af7ea77c3edf9d298f
kazem <kazem>
2024-11-26 17:13:39 -0500

    fix: no headless opencv-python

--- install-client-requirements.sh
+++ install-client-requirements.sh
@@ -12,22 +12,19 @@ function install-opencv-python() {
   git clone --branch ${OPENCV_VER} --depth 1 --recurse-submodules --shallow-submodules https://github.com/opencv/opencv-python.git opencv-python-${OPENCV_VER}
   cd opencv-python-${OPENCV_VER}
 
-  # We want the HEADLESS opencv for GUI stuff
-  export ENABLE_HEADLESS=0
-
   export ENABLE_CONTRIB=0
   # We want GStreamer support enabled.
   export CMAKE_ARGS="-DWITH_GSTREAMER=ON"
 
   python3 -m pip wheel . --verbose
-  python3 -m pip install opencv_python*.whl
+  python3 -m pip install *.whl
 
   popd
 }
 
 if [[ ! -f "./venv/bin/activate" ]]; then
   python3 -m venv venv
-  source venv/bin/activate
+  source ./venv/bin/activate
   pip3 install -r requirements_client.txt
   install-opencv-python
 fi

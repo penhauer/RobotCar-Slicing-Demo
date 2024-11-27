403b50b1632f51aff321e6988a2dda4f17b855b8
kazem <kazem>
2024-11-27 15:05:52 -0500

    fix: fix command in remove-lantecy.sh

--- remove-latency.sh
+++ remove-latency.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 POD=`kubectl get pods -n open5gs | grep upf2 | awk ' { print $1 } '`
-kubectl exec -it "${POD}" -n open5gs -- tc qdisc dev dev eth0 root
+kubectl exec -it "${POD}" -n open5gs -- tc qdisc del dev eth0 root
